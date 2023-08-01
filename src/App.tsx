2db992f088ded353651fee8696148ce99be2c3ef
EmilySantoos <emily.ramos.11.009@gmail.com>
2023-08-01 20:40:28 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
-import Home from './paginas/home/Home';
 import './App.css';
+
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './page/home/login/Login';
+import Cadastro from './page/home/cadastro/Cadastro';
+import Home from './page/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './components/listaTemas/ListaTemas';
+import FormularioTema from './components/formularioTema/FormularioTema';
+import DeletarTema from './components/deletarTemas/DeletarTemas';
 
 
 function App() {
   return (
-   <Home />
+    <>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
+    </>
   );
 }
-
 export default App;
